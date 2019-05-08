802433382a4b67156db9d092172b84465ed323d1
Rinalds Domanovs <rinalds.domanovs@gmail.com>
2019-05-08 11:35:25 +0300

    Initial Commit

---
+++ Project2/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  Project2
+//
+//  Created by Rinalds Domanovs on 06/05/2019.
+//  Copyright © 2019 Rinalds Domanovs. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet var button1: UIButton!
+    @IBOutlet var button2: UIButton!
+    @IBOutlet var button3: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
